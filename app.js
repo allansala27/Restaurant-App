f6b955171c358b5f0433f86f6bf470de4f194c5a
Allan Salandanan <Allan@Moniques-MacBook-Pro.local>
2017-04-22 10:52:11 -0700

    started js

---
+++ app.js
@@ -0,0 +1,9 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Port and Express 
+var app = express();
+var PORT = 3000;
+
