ae7da0c01e9b4fcfb6e15202507758f4b1470166
Allan Salandanan <Allan@Moniques-MacBook-Pro.local>
2017-04-22 11:14:39 -0700

    established server

--- app.js
+++ app.js
@@ -7,3 +7,43 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+  type: "application/vnd.api+json"
+}));
+
+// holds list of customers
+var guestList = [];
+
+// new guest constructor
+function Guest (name, phone, email, id) {
+	this.name = name;
+	this.phone = phone;
+	this.email = email;
+	this.id = id
+}
+
+
+var makeReservation = function () {};
+// Get values from user
+// calls gest constructor 
+// Push into array - list of reservations 
+
+
+var viewTable = function () {};
+
+// LISTENER =================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
+// ROUTES ===================================
+//
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
